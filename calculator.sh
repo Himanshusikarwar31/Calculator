d177519a374b1a88f51cb011c7b2ca33750453bc
Himanshusikarwar31 <hsikarwar02@gmail.com>
2026-03-24 01:59:31 +0530

    Update calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -3,7 +3,15 @@
 # Simple Calculator Script
 
 echo "Enter first number:"
+<<<<<<< HEAD
+<<<<<<< HEAD
 read num200
+=======
+read num2000
+>>>>>>> 7716f8c79875806483ef8f226f398018f4fb20aa
+=======
+read num200
+>>>>>>> d288c0032b29134dc3b83c03da1043fdca7ee7ed
 
 echo "Enter second number:"
 read num4
